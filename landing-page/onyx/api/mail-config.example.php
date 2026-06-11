4122e13fe7e4539744e293baa8171e5425937093
agni-devG <akshatdx12345@gmail.com>
2026-06-11 12:53:55 +0530

    added onyx landing page

---
+++ landing-page/onyx/api/mail-config.example.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'smtp_host' => 'smtp.gmail.com',
+    'smtp_port' => 587,
+    'smtp_user' => 'your-email@gmail.com',
+    'smtp_pass' => 'your-gmail-app-password',
+    'smtp_secure' => 'tls',
+    'mail_to' => '919605970555@beepmate.io',
+    'mail_to_name' => 'Onyx',
+];
